2f422ebf9660c1e5b93a5d0282d46d5c29ddad9b
leeseungmin4966 <bae8416o@naver.com>
2020-05-21 14:50:56 +0000

    improved

--- V2_main.py
+++ V2_main.py
@@ -1,5 +1,5 @@
-from indeed import get_jobs as get_indeed_jobs
-from so import get_jobs as get_so_jobs
+from V2_indeed import get_jobs as get_indeed_jobs
+from V2_so import get_jobs as get_so_jobs
 so_jobs = get_so_jobs()
 indeed_jobs = get_indeed_jobs()
 jobs = indeed_jobs + so_jobs
