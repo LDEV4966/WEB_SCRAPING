386ad6360c09b058aca35478ad868b67fa861a2f
leeseungmin4966 <bae8416o@naver.com>
2020-05-21 14:55:01 +0000

    Version2

--- V2_main.py
+++ V2_main.py
@@ -1,6 +1,7 @@
 from V2_indeed import get_jobs as get_indeed_jobs
 from V2_so import get_jobs as get_so_jobs
-so_jobs = get_so_jobs()
 indeed_jobs = get_indeed_jobs()
+so_jobs = get_so_jobs()
 jobs = indeed_jobs + so_jobs
 print(jobs)
+#앞으로 excel에 넣을작업이 있을거임
